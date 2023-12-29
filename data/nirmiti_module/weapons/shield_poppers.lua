38bb3fac459338ad4b0832107a471103065a23dc
Alder <139022720+AlderStoresWordsOfMagic@users.noreply.github.com>
2023-12-28 17:55:13 -0700

    Added tagification logic (XML to Lua) and implemented two custom tags.

--- data/nirmiti_module/weapons/shield_poppers.lua
+++ data/nirmiti_module/weapons/shield_poppers.lua
@@ -1,13 +1,39 @@
 -- [Shield Poppers - Manually pop shields with weapons. Contains code for both shield and hull collisions.] --
 
-mods.nirmiti.popShield = {}
-local popShield = mods.nirmiti.popShield
+if not mods.nirmiti.implementedShieldPoppers then
+mods.nirmiti.implementedShieldPoppers = true
+
+-- [/// CORE IMPORTS ///] --
+
+local parse_xml_bool = mods.nirmiti.parse_xml_bool
+local node_children = mods.nirmiti.node_children
+local node_get_value = mods.nirmiti.node_get_value
+local node_get_value_default = mods.nirmiti.node_get_value_default
+local node_get_number = mods.nirmiti.node_get_number
+local node_get_number_default = mods.nirmiti.node_get_number_default
+local node_get_bool_default = mods.nirmiti.node_get_bool_default
+local bp_tag_data = mods.nirmiti.bp_tag_data
+local bp_tag_add = mods.nirmiti.bp_tag_add
+
+-- [/// PARSER ///] --
+
+local function parser(node, bpName)
+    local popData = {
+        type = node_get_value_default(node:first_attribute("type"), "pop"),
+        count = node_get_number_default(node:first_attribute("count"), 0),
+        countSuper = node_get_number_default(node:first_attribute("countSuper"), 0),
+        crush = node_get_number_default(node:first_attribute("crush"), 0)
+    }
+    return popData
+end
+
+-- [/// LOGIC /// ] ---
 
 script.on_internal_event(Defines.InternalEvents.SHIELD_COLLISION, function(shipManager, projectile, damage, response)
     if projectile.bDamageSuperShield == nil or projectile.bDamageSuperShield then
         local shieldPower = shipManager.shieldSystem.shields.power
-        local popData = nil
-        if pcall(function() popData = popShield[Hyperspace.Get_Projectile_Extend(projectile).name] end) and popData then
+        local popData = bp_tag_data(projectile.extend.name, "shieldPopper")
+        if popData and (popData["type"] == "pop") then
             if shieldPower.super.first > 0 then
                 if popData.countSuper > 0 then
                     shipManager.shieldSystem:CollisionReal(projectile.position.x, projectile.position.y, Hyperspace.Damage(), true)
@@ -18,26 +44,29 @@ script.on_internal_event(Defines.InternalEvents.SHIELD_COLLISION, function(shipM
                 shieldPower.first = math.max(0, shieldPower.first - popData.count)
                 if shieldPower.first == 0 then
                     shieldPower.first = shieldPower.first - popData.crush
-                    Hyperspace.Global.GetInstance():GetSoundControl():PlaySoundMix("nirmiti_shield_crush",1,true) -- [TODO: make sound changeable]
+                    Hyperspace.Global.GetInstance():GetSoundControl():PlaySoundMix("nirmiti_shield_crush", 1, true) -- [TODO: make sound changeable]
                 end
             end
         end
     end
 end)
 
-mods.nirmiti.popHull = {}
-local popHull = mods.nirmiti.popBallistics
-
 script.on_internal_event(Defines.InternalEvents.DAMAGE_AREA_HIT, function(shipManager, projectile, location, damage, shipFriendlyFire)
     local shieldSystem = shipManager.shieldSystem
-    local popData = nil
-    if shieldSystem and pcall(function() popData = popHull[Hyperspace.Get_Projectile_Extend(projectile).name] end) and popData then
+    local popData = bp_tag_data(projectile.extend.name, "shieldPopper")
+    if shieldSystem and popData and popData["type"] == "pierce" then
         shieldSystem:CollisionReal(location.x, location.y, Hyperspace.Damage(), true)
         shieldSystem.shields.power.first = math.max(0, shieldSystem.shields.power.first - popData.count) 
         if shieldSystem.shields.power.first == 0 then
             shieldSystem.shields.power.first = shieldSystem.shields.power.first - popData.crush
-            Hyperspace.Global.GetInstance():GetSoundControl():PlaySoundMix("nirmiti_shield_crush",1,true) -- [TODO: make sound changeable]
+            Hyperspace.Global.GetInstance():GetSoundControl():PlaySoundMix("nirmiti_shield_crush", 1, true) -- [TODO: make sound changeable]
         end
     end
     return Defines.Chain.CONTINUE
 end)
+
+-- [/// ADD TAG ///] --
+
+bp_tag_add("weaponBlueprint", "shieldPopper", parser)
+
+end
